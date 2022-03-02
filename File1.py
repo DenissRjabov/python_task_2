cd24027d25bc3af03b6a52632f27c28c4a4ded67
Denis <deniss777@live.ru>
2022-03-02 19:09:04 +0200

    Initial commit

---
+++ File1.py
@@ -0,0 +1,8 @@
+letter = input('enter letter of a latin alfabet')
+if letter == 'a' or letter == 'e' or letter == 'i' or \
+        letter == 'o' or letter== 'u' :
+    print('wovel letters')
+elif letter == 'y':
+    print('sometime wolver sometime consonant')
+else:
+    print('this is consonant letter')
